ba1afeec750ffed6c00f820d3dca2f5877a18a17
juanachopanacho <juanchozamoramachuca@gmail.com>
2023-07-24 23:37:18 -0500

    completado

--- script.js
+++ script.js
@@ -88,3 +88,39 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", this.window.scrollY > 50)
 })
+
+// togle icon bar
+
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
+
+// parallax
+
+const observer = new IntersectionObserver ((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        } else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
